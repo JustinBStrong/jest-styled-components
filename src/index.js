53f38f2b1f87f16060b76457344b6e85686c89d1
JustinBStrong <justinbenstrong@gmail.com>
2023-12-15 00:11:40 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const toHaveStyleRule = require('./toHaveStyleRule');
 const styleSheetSerializer = require('./styleSheetSerializer');
 const { resetStyleSheet } = require('./utils');
-
+let test = ''
 global.beforeEach(resetStyleSheet);
 
 expect.addSnapshotSerializer(styleSheetSerializer);
